946f289741c0d19870ecdfd27296d112797e75c5
AlexanderKnapik <s3543757@student.rmit.edu.au>
2018-04-17 20:18:30 +1000

    Work on new defining area activity. Mostly just placeholder stuff for now.

---
+++ AnimalsAtRisk/app/src/main/java/com/s3543757/alexanderknapik/animalsatrisk/areasDefine.java
@@ -0,0 +1,13 @@
+package com.s3543757.alexanderknapik.animalsatrisk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class areasDefine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_areas_define);
+    }
+}
